140c50135fdfdeebf45daf2b7a4909dd6e60d53f
Gabriel Gomes Rodrigues <138343762+gabrielgr200@users.noreply.github.com>
2023-07-25 18:58:36 -0300

    api

---
+++ controllers/users.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../db/models');
+const jwt = require('jsonwebtoken');
+
+router.post("/register", async (req, res) =>{
+
+    var dados = req.body;
+    console.log(dados);
+
+    await db.cadastro.create(dados).then((dadosUsuario) =>{
+        return res.json({
+            mensagem: "Usuario cadastrado com sucesso",
+            dadosUsuario
+        });
+    }).catch(() =>{
+        return res.json({
+            mensagem: "Usuario não foi cadastrado"
+        });
+    });
+
+      
+});
+
+router.get("/users", async (req, res) =>{
+    const users = await db.cadastro.findAll({
+
+        attributes: ['id', 'name', 'email'],
+
+        order: [['id', 'DESC']]
+    });
+     
+    if(users){
+        return res.json({
+            users
+        });
+    }else{
+        return res.status(400).json({
+            mensagem: "Usuario não foi cadastrado"
+        });
+    }
+
+});
+
+router.post("/login", async (req, res) => {
+    const { identifier, password } = req.body;
+    let user;
+
+    if (identifier.includes('@')) {
+        
+        user = await db.cadastro.findOne({ where: { email: identifier } });
+    } else {
+        
+        user = await db.cadastro.findOne({ where: { name: identifier } });
+    }
+
+    if (!user) {
+        return res.status(401).json({
+            mensagem: "Credenciais inválidas"
+        });
+    }
+
+    if (user.password !== password) {
+        return res.status(401).json({
+            mensagem: "Credenciais inválidas"
+        });
+    }
+
+
+    return res.json({
+        mensagem: "Login bem-sucedido"
+    });
+});
+
+
+router.get("/users", async (req, res) => {
+  
+    const users = await db.cadastro.findAll({
+        attributes: ['id', 'name', 'email'],
+        order: [['id', 'DESC']]
+    });
+
+    if (users) {
+        return res.json({
+            users
+        });
+    } else {
+        return res.status(400).json({
+            mensagem: "Nenhum usuário encontrado"
+        });
+    }
+});
+
+module.exports = router;
