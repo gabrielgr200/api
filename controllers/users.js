b7b05ea7c57076fc9c5b32387b89b577e70f2d5f
Gabriel Gomes Rodrigues <138343762+gabrielgr200@users.noreply.github.com>
2023-07-30 12:15:57 -0300

    api

--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./../db/models');
-const jwt = require('jsonwebtoken');
 
-const SECRET_KEY = 'U3#2w$Gs9aBv^67z8Nl!Tq1m5Op&*JkR';
-
-const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
-
-  if (!token) {
-    return res.status(403).json({ mensagem: 'Token não fornecido' });
-  }
-
-  try {
-    const decoded = jwt.verify(token, SECRET_KEY);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ mensagem: 'Token inválido' });
-  }
-};
 
 router.post("/register", async (req, res) => {
   var dados = req.body;
@@ -27,16 +9,10 @@ router.post("/register", async (req, res) => {
 
   try {
     const dadosUsuario = await db.cadastro.create(dados);
-    const token = jwt.sign(
-      { id: dadosUsuario.id, name: dadosUsuario.name, email: dadosUsuario.email },
-      SECRET_KEY,
-      { expiresIn: '7d' }
-    );
 
     return res.json({
       mensagem: "Usuario cadastrado com sucesso",
       dadosUsuario,
-      token 
     });
   } catch (err) {
     return res.json({
@@ -45,31 +21,19 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.get("/users", verifyToken, async (req, res) => {
+router.get("/users", async (req, res) => {
   try {
-    if (!req.user || !req.user.id) {
-      return res.status(401).json({ mensagem: "Acesso não autorizado" });
-    }
-
-    const userId = req.user.id;
-
-    const user = await db.cadastro.findByPk(userId, {
+    const users = await db.cadastro.findAll({
       attributes: ['id', 'name', 'email', 'password']
     });
 
-    if (user) {
-      return res.json({
-        user
-      });
-    } else {
-      return res.status(404).json({
-        mensagem: "Usuário não encontrado"
-      });
-    }
+    return res.json({
+      users
+    });
   } catch (err) {
     console.error(err);
     return res.status(500).json({
-      mensagem: "Erro ao buscar usuário"
+      mensagem: "Erro ao buscar usuários"
     });
   }
 });
@@ -95,15 +59,9 @@ router.post("/login", async (req, res) => {
       mensagem: "Credenciais inválidas"
     });
   }
-  const token = jwt.sign(
-    { id: user.id, name: user.name, email: user.email },
-    SECRET_KEY,
-    { expiresIn: '7d' }
-  );
 
   return res.json({
     mensagem: "Login bem-sucedido",
-    token
   });
 });
 
