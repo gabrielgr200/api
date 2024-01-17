1c0d79ae929376e06c99ee6287b0bdf4272d9158
Leirbag <gomesrdg34@gmail.com>
2024-01-17 19:09:03 -0300

    register

--- api.js
+++ api.js
@@ -3,9 +3,11 @@ const api = express();
 const db = require("./db/models");
 const users = require('./controllers/users');
 
+const port = 3440
+
 api.use(express.json());
 api.use('/', users);
 
-api.listen(3440, () => {
+api.listen(process.env.PORT || port, () => {
     console.log("teste: http://localhost:3440");
 });
